b55aea1930d4536f89120fb4f41343ef2fc4f417
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-22 14:21:04 -0300

    Create util's common constants file

---
+++ app/src/main/java/com/mvclopes/wtest/utils/CommonConstants.kt
@@ -0,0 +1,5 @@
+package com.mvclopes.wtest.utils
+
+const val COMMA_SEPARATOR = ","
+const val CSV_FILE_NAME = "cod-postais.csv"
+const val URL_CSV_FILE = "https://raw.githubusercontent.com/centraldedados/codigos_postais/master/data/codigos_postais.csv"

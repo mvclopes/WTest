7cc2d82a4e60a744a70c98f5bc41b0b513c0eb9e
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-22 14:20:09 -0300

    Create datasource layer

---
+++ app/src/main/java/com/mvclopes/wtest/data/datasource/PostalCodeDatasource.kt
@@ -0,0 +1,10 @@
+package com.mvclopes.wtest.data.datasource
+
+import com.mvclopes.wtest.data.datasource.local.entity.PostalCodeEntity
+import kotlinx.coroutines.flow.Flow
+
+interface PostalCodeDatasource {
+    fun getAll(): Flow<List<PostalCodeEntity>>
+    fun insertPostalCode(postalCode: PostalCodeEntity): Flow<Unit>
+    fun insertAll(postalCodeList: List<PostalCodeEntity>): Flow<Unit>
+}
---
+++ app/src/main/java/com/mvclopes/wtest/data/datasource/PostalCodeDatasourceImpl.kt
@@ -0,0 +1,23 @@
+package com.mvclopes.wtest.data.datasource
+
+import com.mvclopes.wtest.data.datasource.local.dao.PostalCodeDao
+import com.mvclopes.wtest.data.datasource.local.entity.PostalCodeEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class PostalCodeDatasourceImpl (
+    private val postalCodeDao: PostalCodeDao
+) : PostalCodeDatasource {
+    override fun getAll(): Flow<List<PostalCodeEntity>> {
+        return flow { emit(postalCodeDao.getAll()) }
+    }
+
+    override fun insertPostalCode(postalCode: PostalCodeEntity): Flow<Unit> {
+        return flow { emit(postalCodeDao.insertPostalCode(postalCode)) }
+    }
+
+    override fun insertAll(postalCodeList: List<PostalCodeEntity>): Flow<Unit> {
+        return flow { emit(postalCodeDao.insertAll(postalCodeList)) }
+    }
+
+}

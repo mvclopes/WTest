88208ba214d2c8b5740740a349ca64d93c9afbce
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-22 14:18:49 -0300

    Create postal code model

--- app/src/main/java/com/mvclopes/wtest/domain/model/PostalCode.kt
+++ app/src/main/java/com/mvclopes/wtest/domain/model/PostalCode.kt
@@ -1,6 +1,9 @@
 package com.mvclopes.wtest.domain.model
 
 data class PostalCode(
+    val localityCode: String,
+    val localityName: String,
     val postalCodeNumber: String,
+    val postalCodeExtensionNumber: String,
     val postalDesignation: String
 )

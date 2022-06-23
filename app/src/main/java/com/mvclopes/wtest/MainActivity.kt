15a0cee860419bdc6477629b8ea3cbf7d3d3579a
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-22 22:47:12 -0300

    Refactored way to validate if file already exists

--- app/src/main/java/com/mvclopes/wtest/MainActivity.kt
+++ app/src/main/java/com/mvclopes/wtest/MainActivity.kt
@@ -5,15 +5,14 @@ import android.app.DownloadManager
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import java.io.File
+import com.mvclopes.wtest.utils.CSV_DIR_NAME
+import com.mvclopes.wtest.utils.CSV_FILE_NAME
+import com.mvclopes.wtest.utils.URL_CSV_FILE
 
 private const val PERMISSION_REQUESTED = Manifest.permission.READ_EXTERNAL_STORAGE
-private const val URL_CSV_FILE = "https://raw.githubusercontent.com/centraldedados/codigos_postais/master/data/codigos_postais.csv"
-private const val CSV_FILE_NAME = "cod-postais.csv"
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,26 +39,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun verifyFile() {
-        val listFiles: Array<out File>? = Environment
-            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                .listFiles()
+        val fileAlreadyExists = getExternalFilesDir(CSV_DIR_NAME)
+            ?.listFiles()
+            .isNullOrEmpty()
+            .not()
 
-        val fileAlreadyExists: List<File>? =
-            if (listFiles.isNullOrEmpty()) null
-            else listFiles.filter { it.name == CSV_FILE_NAME }
-
-        if (fileAlreadyExists.isNullOrEmpty()) {
+        if (!fileAlreadyExists) {
             downloadCSV()
         }
     }
 
     private fun downloadCSV() {
         val request = DownloadManager.Request(Uri.parse(URL_CSV_FILE))
-            .setTitle("codigos-postais")
+            .setTitle(CSV_FILE_NAME)
             .setDescription("Downloading...")
             .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
             .setAllowedOverMetered(true)
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, CSV_FILE_NAME)
+            .setDestinationInExternalFilesDir(this, CSV_DIR_NAME, CSV_FILE_NAME)
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadManager.enqueue(request)
     }
--- app/src/main/java/com/mvclopes/wtest/utils/CommonConstants.kt
+++ app/src/main/java/com/mvclopes/wtest/utils/CommonConstants.kt
@@ -2,4 +2,5 @@ package com.mvclopes.wtest.utils
 
 const val COMMA_SEPARATOR = ","
 const val CSV_FILE_NAME = "cod-postais.csv"
+const val CSV_DIR_NAME = "postal-code"
 const val URL_CSV_FILE = "https://raw.githubusercontent.com/centraldedados/codigos_postais/master/data/codigos_postais.csv"

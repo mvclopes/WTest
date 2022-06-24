afa85dca05a291ee83ec4d09e1f0912f362d733b
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-24 15:41:41 -0300

    Refactored DI module

--- app/src/main/java/com/mvclopes/wtest/di/PostalCodeModule.kt
+++ app/src/main/java/com/mvclopes/wtest/di/PostalCodeModule.kt
@@ -1,12 +1,8 @@
 package com.mvclopes.wtest.di
 
 import com.mvclopes.wtest.data.datasource.PostalCodeDatasourceImpl
-import com.mvclopes.wtest.data.datasource.local.db.PostalCodeDatabase
 import com.mvclopes.wtest.data.datasource.local.db.getDataBase
 import com.mvclopes.wtest.data.repository.PostalCodeRepositoryImpl
-import com.mvclopes.wtest.domain.usecase.GetPostalCodesUseCase
-import com.mvclopes.wtest.domain.usecase.InsertPostalCodeUseCase
-import com.mvclopes.wtest.domain.usecase.IsDatabaseEmptyUseCase
 import com.mvclopes.wtest.domain.usecase.ReadCsvFileUseCase
 import com.mvclopes.wtest.presentation.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -17,24 +13,17 @@ val postalCodeModule = module {
     viewModel {
         HomeViewModel(
             readCsvFileUseCase = getReadCsvFileUseCase(),
-            isDatabaseEmptyUseCase = isDatabaseEmptyUseCase(),
-            getPostalCodesUseCase = getPostalCodesUseCase(),
-            insertPostalCodeUseCase = getInsertPostalCodeUseCase(),
+            repository = getPostalCodeRepository(),
         )
     }
-    single { getDataBase(context = get()) }
 }
 
-private fun Scope.getPostalCodeDAO() = get<PostalCodeDatabase>().postalCodeDAO()
+private fun Scope.getPostalCodeDatabase() = getDataBase(context = get())
+
+private fun Scope.getPostalCodeDAO() = getPostalCodeDatabase().postalCodeDAO()
 
 private fun Scope.getPostalCodeDataSource() = PostalCodeDatasourceImpl(getPostalCodeDAO())
 
 private fun Scope.getPostalCodeRepository() = PostalCodeRepositoryImpl(getPostalCodeDataSource())
 
 private fun Scope.getReadCsvFileUseCase() = ReadCsvFileUseCase(context = get())
-
-private fun Scope.getPostalCodesUseCase() = GetPostalCodesUseCase(repository = getPostalCodeRepository())
-
-private fun Scope.isDatabaseEmptyUseCase() = IsDatabaseEmptyUseCase(repository = getPostalCodeRepository())
-
-private fun Scope.getInsertPostalCodeUseCase() = InsertPostalCodeUseCase(repository = getPostalCodeRepository())

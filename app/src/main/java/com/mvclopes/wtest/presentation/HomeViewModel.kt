6985ce7f487c96ca0a73840af483883d7aa69b50
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-24 15:42:19 -0300

    Created home viewmModel

---
+++ app/src/main/java/com/mvclopes/wtest/presentation/HomeViewModel.kt
@@ -0,0 +1,81 @@
+package com.mvclopes.wtest.presentation
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mvclopes.wtest.domain.model.PostalCode
+import com.mvclopes.wtest.domain.repository.PostalCodeRepository
+import com.mvclopes.wtest.domain.usecase.ReadCsvFileUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val readCsvFileUseCase: ReadCsvFileUseCase,
+    private val repository: PostalCodeRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+): ViewModel() {
+
+    private var _postalCodeList = MutableLiveData<List<PostalCode>>()
+    val postalCodeList: LiveData<List<PostalCode>>
+        get() = _postalCodeList
+
+    private var _isLoading = MutableLiveData(true)
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    fun isDatabaseEmpty() {
+        viewModelScope.launch {
+            repository.isDatabaseEmpty()
+                .flowOn(dispatcher)
+                .onStart { isLoading(true) }
+                .catch { Log.i("TAG_ViewModel", "exception: ${it.message}") }
+                .collect { handleIsDatabaseEmpty(it) }
+        }
+    }
+
+    private fun isLoading(isLoading: Boolean) {
+        _isLoading.value = isLoading
+    }
+
+    private fun handleIsDatabaseEmpty(isEmpty: Boolean) {
+        Log.i("TAG_", "IsDatabaseEmpty: $isEmpty")
+        when (isEmpty) {
+            true -> readCSV()
+            else -> fetchData()
+        }
+    }
+
+    private fun readCSV() {
+        val postalCodeList = readCsvFileUseCase()
+        viewModelScope.launch(dispatcher) {
+            try {
+                repository.insertAll(postalCodeList).collect { fetchData() }
+                Log.i("TAG_ViewModel", "recorded data")
+            } catch (e: Exception) {
+                Log.i("TAG_ViewModel", "exception inserting data: ${e.message}")
+            }
+        }
+    }
+
+    private fun fetchData() {
+        viewModelScope.launch {
+            repository.getAll()
+                .flowOn(dispatcher)
+                .onCompletion { isLoading(false) }
+                .catch { Log.i("TAG_ViewModel", "exception getting data: ${it.message}") }
+                .collect {
+                    _postalCodeList.value = it
+                    Log.i("TAG_ViewModel", "read data [size]: ${it.size}")
+                }
+        }
+    }
+
+}

137dcd47e7f706d3e443d4950cd39c3c0e91fd01
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-25 11:41:55 -0300

    Created use case to verify custom search query

--- app/src/main/java/com/mvclopes/wtest/domain/usecase/VerifyCustomQueryUseCase.kt
+++ app/src/main/java/com/mvclopes/wtest/domain/usecase/VerifyCustomQueryUseCase.kt
@@ -1,16 +1,15 @@
 package com.mvclopes.wtest.domain.usecase
 
 import com.mvclopes.wtest.utils.CustomQuery
+import com.mvclopes.wtest.utils.numberRegex
+import com.mvclopes.wtest.utils.textRegex
 
 class VerifyCustomQueryUseCase {
 
-    private val textRegex = "[a-zA-Z\\u00C0-\\u024F]+".toRegex()
-    private val numberRegex = "[0-9-]+".toRegex()
-
     operator fun invoke(searchTerm: String): CustomQuery {
         return when {
-            searchTerm.matches(textRegex) -> CustomQuery.TextQuery
-            searchTerm.matches(numberRegex) -> CustomQuery.NumberQuery
+            searchTerm.matches(textRegex()) -> CustomQuery.TextQuery
+            searchTerm.matches(numberRegex()) -> CustomQuery.NumberQuery
             else -> CustomQuery.TextAndNumberQuery
         }
     }

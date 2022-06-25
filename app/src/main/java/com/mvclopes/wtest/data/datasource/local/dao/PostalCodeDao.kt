00b22ce42d65139530f819c1c86b84b13ce56155
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-25 11:40:23 -0300

    Added new methods to dao: custom search queries

--- app/src/main/java/com/mvclopes/wtest/data/datasource/local/dao/PostalCodeDao.kt
+++ app/src/main/java/com/mvclopes/wtest/data/datasource/local/dao/PostalCodeDao.kt
@@ -20,4 +20,16 @@ interface PostalCodeDao {
 
     @Query("SELECT * FROM tb_cod_postal LIMIT 1")
     suspend fun isDatabaseEmpty(): PostalCodeEntity?
+
+    @Query("SELECT * FROM tb_cod_postal WHERE num_cod_postal LIKE :postalCodeNumber")
+    suspend fun searchByPostalCodeNumber(postalCodeNumber: String): List<PostalCodeEntity>
+
+    @Query("SELECT * FROM tb_cod_postal WHERE desig_postal LIKE :postalDesignation")
+    suspend fun searchByPostalDesignation(postalDesignation: String): List<PostalCodeEntity>
+
+    @Query("SELECT * FROM tb_cod_postal WHERE num_cod_postal LIKE :postalCodeNumber AND desig_postal LIKE :postalDesignation")
+    suspend fun searchByDesignationAndCodeNumber(
+        postalCodeNumber: String,
+        postalDesignation: String
+    ): List<PostalCodeEntity>
 }
